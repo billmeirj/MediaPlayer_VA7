7ade64ead659fe5fbed267661a5c525c6baf21e5
abill <abill@LAPTOP-V7SUI5GT.fritz.box>
2026-06-05 16:49:20 +0200

    add code from subtask B

--- src/studiplayer/ui/Player.java
+++ src/studiplayer/ui/Player.java
@@ -1,37 +1,72 @@
 package studiplayer.ui;
+import java.io.File;
+
 import javafx.application.*;
 import javafx.scene.*;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import studiplayer.audio.NotPlayableException;
 import studiplayer.audio.PlayList;
+import studiplayer.audio.NotPlayableException;
 
 public class Player extends Application {
 
 	private PlayList playList;
 	private boolean useCertPlayList;
+	public final static String DEFAULT_PLAYLIST ="playlists/DefaultPlayList.m3u";
+	
 	
 	public Player () {
 		
 	}
 	
-	public final static String DEFAULT_PLAYLIST () {
-		return "";
+	public void setUseCertPlayList (boolean value) {
+		this.useCertPlayList = value;
 	}
 	
-	public void setUserCertPlayList (boolean value) {
-		value = this.useCertPlayList;
-	}
-	
+	//Playlist setzen
 	public void setPlayList (String pathname) {
-		this.playList = new PlayList(pathname);
+		loadPlayList(pathname);
 	}
 	
+	//falls playlist null oder leer ist
+	public void loadPlayList (String pathname) {
+		if (pathname == null || pathname.isEmpty()) {
+			pathname = DEFAULT_PLAYLIST;
+		}
+		try {
+			this.playList = new PlayList(pathname);
+		} catch (Exception e) {
+			System.err.println("Fehler beim Laden der Playlist: " + e.getMessage());
+		}
+		
+	}
+	
+	//Java FX starten
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
+	
+	//Fenster aufrufen
 	@Override
 	public void start(Stage stage) {
+		if (this.useCertPlayList) {
+			loadPlayList("playList.cert.m3u");
+		} else {
+			FileChooser fileChooser = new FileChooser();
+			
+			File selectedFile = fileChooser.showOpenDialog(stage);
+			
+			if (selectedFile != null) {
+				String currentFile = selectedFile.getAbsolutePath();
+				loadPlayList(currentFile);
+			} else {
+				loadPlayList(null);
+			}
+		}
+		
 		BorderPane hauptPane = new BorderPane();
 		Scene scene = new Scene(hauptPane, 600, 400);
 		
